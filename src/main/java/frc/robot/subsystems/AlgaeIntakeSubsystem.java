afb4dbb83b71a4ec24ff61143fafb91989fde8c7
jonathan berent <jonathan.berent@vsolvit.com>
2025-02-15 13:10:02 -0500

    add default commands

--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -1,7 +1,13 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.team1502.configuration.annotations.DefaultCommand;
+import org.team1502.configuration.annotations.SubsystemInfo;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.AlgaeIntakeCommands;
+
+@SubsystemInfo(disabled = false)
+@DefaultCommand(command = AlgaeIntakeCommands.class)
 public class AlgaeIntakeSubsystem extends SubsystemBase {
     
     
--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -1,11 +1,16 @@
 package frc.robot.subsystems;
 
+import org.team1502.configuration.annotations.DefaultCommand;
+import org.team1502.configuration.annotations.SubsystemInfo;
 import org.team1502.configuration.factory.RobotConfiguration;
 
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.CoralIntakeCommands;
 
+@SubsystemInfo(disabled = false)
+@DefaultCommand(command = CoralIntakeCommands.class)
 public class CoralIntakeSubsystem extends SubsystemBase {
     final SparkMax m_motor;
     public CoralIntakeSubsystem(RobotConfiguration robotConfiguration) {
