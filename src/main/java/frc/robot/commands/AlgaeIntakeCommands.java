d6d031f558e5acf0564e7fbe97f5d142aaa898c5
jonathan berent <jonathan.berent@vsolvit.com>
2025-02-15 12:38:18 -0500

    add algae and coral commands

---
+++ src/main/java/frc/robot/commands/AlgaeIntakeCommands.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.CoralIntakeSubsystem;
+import frc.robot.team1502.Operator;
+
+public class AlgaeIntakeCommands extends Command
+    private final AlgaeIntakeSubsystem m_subsystem;
+    
+    public AlgaeIntakeCommands(AlgaeIntakeSubsystem subsystem) {
+        m_subsystem = subsystem;
+        addRequirements(m_subsystem);
+    }
+
+    @Override
+    public void initialize(){
+        //m_subsystem.reset();
+
+        Operator.X
+            .onTrue(new InstantCommand(() -> m_subsystem.in()))
+            .onFalse(new InstantCommand(() -> m_subsystem.stop()));
+        Operator.Y
+            .onTrue(new InstantCommand(() -> m_subsystem.out()))
+            .onFalse(new InstantCommand(() -> m_subsystem.stop()));
+
+    }
+
+    @Override
+    public void execute(){
+    }
+    
+ {
+    
+}
