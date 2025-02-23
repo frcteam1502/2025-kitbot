db4a3142aa64ef5f73f222e80ee4414a25edec96
jonathan berent <jonathan.berent@dx-fx.com>
2025-02-23 08:41:07 -0500

    ENABLE Drive; DISABLE Elevator, Coral

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -10,7 +10,7 @@ import com.revrobotics.spark.SparkBase.ControlType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commands.ElevatorCommands;
 
-@SubsystemInfo(disabled = false)
+@SubsystemInfo(disabled = true)
 @DefaultCommand(command = ElevatorCommands.class)
 public class ElevatorSubsystem extends SubsystemBase {
     final SparkMax m_motor;
