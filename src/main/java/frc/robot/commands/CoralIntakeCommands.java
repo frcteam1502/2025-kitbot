2104b3a1037a98941f551b7871d707bcf65a21f8
jonathan berent <jonathan.berent@vsolvit.com>
2025-03-15 12:02:22 -0400

    WIP add coral rotate FF

--- src/main/java/frc/robot/commands/CoralIntakeCommands.java
+++ src/main/java/frc/robot/commands/CoralIntakeCommands.java
@@ -1,5 +1,8 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.CoralIntakeSubsystem;
@@ -7,14 +10,16 @@ import frc.robot.team1502.Operator;
 
 public class CoralIntakeCommands extends Command {
     private final CoralIntakeSubsystem m_subsystem;
-    
+        
+    ArmFeedforward m_feedforward = new ArmFeedforward(0, 0.8, 0.5 );
+
     public CoralIntakeCommands(CoralIntakeSubsystem subsystem) {
         m_subsystem = subsystem;
         addRequirements(m_subsystem);
     }
 
     @Override
-    public void initialize(){
+    public void initialize()
         //m_subsystem.reset();
 
         Operator.X//eat
@@ -23,14 +28,28 @@ public class CoralIntakeCommands extends Command {
         Operator.B//arf
             .onTrue(new InstantCommand(() -> m_subsystem.out()))
             .onFalse(new InstantCommand(() -> m_subsystem.stop()));
-        Operator.West
-            .onTrue(new InstantCommand(() -> m_subsystem.setPos(0.547)));
+        
+        Operator.West.onTrue(new InstantCommand(() -> m_subsystem.setPos(0.547)));
 
     }
-
+    double setpoint = 0.0;
+    
     @Override
     public void execute(){
-        m_subsystem.rotate(Operator.getRightTrigger()-Operator.getLeftTrigger());
+
+        double move = Operator.getRightTrigger() - Operator.getLeftTrigger();
+        setpoint += move; // setpoint = setpoint + move;
+        double position = Units.rotationsToRadians(m_subsystem.getPosition());
+        double velocity = setpoint - position;
+        double feedforward = m_feedforward.calculate(setpoint, velocity);
+        m_subsystem.rotate(feedforward);
+
+        SmartDashboard.putNumber(getName()+":setpoint", setpoint);
+        SmartDashboard.putNumber(getName()+":position", position);
+        SmartDashboard.putNumber(getName()+":err", position - setpoint);
+        SmartDashboard.putNumber(getName()+":velocity", velocity);
+        SmartDashboard.putNumber(getName()+":speed", feedforward);
+        SmartDashboard.putNumber(getName()+":Vg", m_feedforward.getKg() * Math.cos(setpoint));
     }
 
     
--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -8,6 +8,7 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commands.CoralIntakeCommands;
@@ -20,14 +21,31 @@ public class CoralIntakeSubsystem extends SubsystemBase {
     public CoralIntakeSubsystem(RobotConfiguration robotConfiguration) {
         m_intakeMotor = robotConfiguration.MotorController("Intake").buildSparkMax();
         m_rotateMotor = robotConfiguration.MotorController("Rotate").buildSparkMax();
+        m_rotateMotor.getEncoder().setPosition(-0.66);
     }
- @Override 
+    
+    // ArmFeedforward feedforward = new ArmFeedforward(0, 0.8, 0.5 );
+    // double setpoint; // radians
+    // double position; // radians
+
+    @Override 
     public void periodic() {
         SmartDashboard.putNumber("ROTATE", m_rotateMotor.getEncoder().getPosition());
         SmartDashboard.putNumber("ROTATEVOLTAGE", m_rotateMotor.getAppliedOutput());
-        feedforward.calculate(m_rotateMotor.getEncoder().getPosition(), 0.5);
+
+
+        // position = Units.rotationsToRadians(m_rotateMotor.getEncoder().getPosition());
+        // double velocity = setpoint - position;
+        // double speed = feedforward.calculate(setpoint, velocity);
+        // rotate(speed);
     }
-    ArmFeedforward feedforward = new ArmFeedforward(0, 0.8, 473 );
+    
+    public double getPosition() {
+        return Units.rotationsToRadians(m_rotateMotor.getEncoder().getPosition()); 
+    }
+    
+    
+    
     public void in () {
         m_intakeMotor.set(0.2);
     }
